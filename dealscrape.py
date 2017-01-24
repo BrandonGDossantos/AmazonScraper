aed1011b4b0e3de2ee84dff7bc6d9535733d1153
root <root@ip-172-31-33-226.us-west-2.compute.internal>
2017-01-24 23:56:40 +0000

    start of something new, it feels so right here with you

--- dealscrape.py
+++ dealscrape.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-binary = FirefoxBinary('C:\Program Files\Mozilla Firefox')
-driver = webdriver.Firefox(firefox_binary=binary)
+#from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+#binary = FirefoxBinary()
+driver = webdriver.Firefox(executable_path='/home/ubuntu/AmazonScraper/geckodriver')
 driver.get('http://www.amazon.com/gp/goldbox/ref=nav_cs_gb')
 html = driver.page_source
 soup = BeautifulSoup(html)
-for link in soup.find_all('a', href=True):
-	print(link['href'])
+spans = soup.find_all('span', attrs={'id': 'dealTitle'})
+for span in spans:
+	print span.string
+	
