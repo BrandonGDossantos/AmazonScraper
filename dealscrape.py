ffc2238694854452f6a977f87fddffbc2d7bdf70
BrandonGDossantos <bxd7887@rit.edu>
2017-01-26 18:58:20 +0000

    Parsing further

--- dealscrape.py
+++ dealscrape.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python
 from bs4 import BeautifulSoup
 from selenium import webdriver
-#from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-#binary = FirefoxBinary()
 driver = webdriver.Firefox(executable_path='/home/ubuntu/AmazonScraper/geckodriver')
 driver.get('http://www.amazon.com/gp/goldbox/ref=nav_cs_gb')
 html = driver.page_source
 soup = BeautifulSoup(html)
-#data = []
-clean_soup = [span.get_text(strip=True) for span in soup.find_all('span', attrs={'id': 'dealTitle'})]
-#for span in spans:
-#	data.append(span.text)
-#result = ''.join([	
-#print(span.get_text())
-print(clean_soup)
+#clean_soup = [span.get_text(strip=True) for span in soup.find_all('span', attrs={'id': 'dealTitle'})]
+#for item in clean_soup:
+#	print(item + '\n')
+	#print(clean_soup)
+divs = soup.find_all('div', attrs={'class': 'a-row dealContainer dealTile'})
+for div in divs:
+	if div.find('span', attrs={'data-action': 'gbdeal-addtocart'}):
+		tmp = div.find('span', attrs={'id': 'dealTitle'})
+		print(tmp)
+		#print('Yes')
+
+
+
+
+
+
