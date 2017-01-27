3a450b6efc8c5eb450940b663d648e3478116712
BrandonGDossantos <bxd7887@rit.edu>
2017-01-27 04:45:02 +0000

    Attempt: scrape next page

--- dealscrape.py
+++ dealscrape.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python
+import sys
+f = open("output.txt", 'w')
+sys.stdout = f
 from bs4 import BeautifulSoup
 from selenium import webdriver
+page = 'http://www.amazon.com/gp/goldbox/ref=nav_cs_gb'
+#for i in range(10):	
 driver = webdriver.Firefox(executable_path='/home/ubuntu/AmazonScraper/geckodriver')
-driver.get('http://www.amazon.com/gp/goldbox/ref=nav_cs_gb')
+driver.get(page)
 html = driver.page_source
-soup = BeautifulSoup(html)
-#clean_soup = [span.get_text(strip=True) for span in soup.find_all('span', attrs={'id': 'dealTitle'})]
-#for item in clean_soup:
-#	print(item + '\n')
-	#print(clean_soup)
+soup = BeautifulSoup(html, "lxml")
 divs = soup.find_all('div', attrs={'class': 'a-row dealContainer dealTile'})
 for div in divs:
-	if div.find('span', attrs={'data-action': 'gbdeal-addtocart'}):
-		tmp = div.find('span', attrs={'id': 'dealTitle'})
-		print(tmp)
-		#print('Yes')
+	tmp = div.find('span', attrs={'id': 'dealTitle'})
+	print(tmp.get_text(strip=True) + '\n')
+#page = soup.find('a', attrs={'href': '#next'})
+driver.find_element_by_link_text('#next').click()
+print(page)
+driver.quit()
+#driver.get(next)
+f.close()
+
+
 
 
 
