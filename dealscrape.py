36a6e55318e2a03c47997699987fb23a79ae2016
BrandonGDossantos <bxd7887@rit.edu>
2017-01-28 05:56:18 +0000

    working

--- dealscrape.py
+++ dealscrape.py
@@ -1,30 +1,38 @@
 #!/usr/bin/env python
 import sys
+#import urllib2
+#import socket
+from bs4 import BeautifulSoup
+from pyvirtualdisplay import Display
+from selenium import webdriver
+from xvfbwrapper import Xvfb
+#vdisplay = Xvfb(width=1280, height=740, colordepth=16)
+#vdisplay.start()
+display = Display(visible=0, size=(1024, 768))
+display.start()
 f = open("output.txt", 'w')
 sys.stdout = f
-from bs4 import BeautifulSoup
-from selenium import webdriver
-page = 'http://www.amazon.com/gp/goldbox/ref=nav_cs_gb'
-#for i in range(10):	
-driver = webdriver.Firefox(executable_path='/home/ubuntu/AmazonScraper/geckodriver')
-driver.get(page)
+#executable_path='/home/ubuntu/AmazonScraper/geckodriver')
+#try:
+driver = webdriver.Firefox()
+driver.implicitly.wait(30)
+driver.maximize_window()
+driver.get('https://www.amazon.com/gp/goldbox/ref=nav_cs_gb')
 html = driver.page_source
 soup = BeautifulSoup(html, "lxml")
 divs = soup.find_all('div', attrs={'class': 'a-row dealContainer dealTile'})
 for div in divs:
 	tmp = div.find('span', attrs={'id': 'dealTitle'})
-	print(tmp.get_text(strip=True) + '\n')
-#page = soup.find('a', attrs={'href': '#next'})
-driver.find_element_by_link_text('#next').click()
-print(page)
-driver.quit()
-#driver.get(next)
+	#print(tmp.get_text(strip=True) + '\n')
+	print(tmp)
+link = driver.findElement(By.linkText("//a[@href='Next']"))
+print link
+#except URLError, e:
+#	print "Oops, timed out?"
+#except socket.timeout:
+#	print "Timed out!"
+
+driver.close()
+display.stop()
+#vdisplay.stop()
 f.close()
-
-
-
-
-
-
-
-
