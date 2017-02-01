db2336fac84ae3e1d01eaffabc40670bbd51b921
BrandonGDossantos <bxd7887@rit.edu>
2017-02-01 04:02:55 +0000

    Almost there

--- dealscrape.py
+++ dealscrape.py
@@ -2,26 +2,31 @@
 import sys
 import os
 import re
-#import urllib2
-#import socket
+import sqlite3
 from bs4 import BeautifulSoup
 from pyvirtualdisplay import Display
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from xvfbwrapper import Xvfb
-#vdisplay = Xvfb(width=1280, height=740, colordepth=16)
-#vdisplay.start()
 binary = FirefoxBinary('/usr/bin')
 display = Display(visible=0, size=(1024, 768))
 display.start()
 f = open("output.txt", 'w')
 sys.stdout = f
 url = 'https://www.amazon.com/gp/goldbox/ref=nav_cs_gbps_ftr_s-3_bb19_page_1?gb_f_GB-SUPPLE=page:1'
-#url = 'https://www.amazon.com/gp/goldbox/ref=nav_cs_gb'
 driver = webdriver.Firefox(executable_path='/home/ubuntu/AmazonScraper/geckodriver')
 driver.maximize_window()
 driver.get(url)
-print('\t' + "Page: 1")
+
+db = sqlite3.connect('amazon.db')
+c = db.cursor()
+c.execute("CREATE TABLE IF NOT EXISTS products (title TEXT, price INT, review INT, link TEXT)")
+db.commit()
+
+
+
+
+#print('\t' + "Page: 1")
 try:
 	for i in range(10):
 		print(url)
@@ -32,27 +37,32 @@ try:
 			tmp = div.find('span', attrs={'id': 'dealTitle'})
 			
 			links = div.find('a', attrs={'class': 'a-link-normal'})
-			cash = div.find('span', attrs={'class': 'a-size-medium a-color-base inlineBlock unitLineHeight'})
+			prices = div.find('span', attrs={'class': 'a-size-medium a-color-base inlineBlock unitLineHeight'})
 			reviews = div.find('span', attrs={'id': 'totalReviews'})
-			print(tmp.get_text(strip=True).encode('utf-8'))	
-			
-			print(links['href'])
-			if cash == None:
-				print('\t' + "None")
+			title = tmp.get_text(strip=True).encode('utf-8')	
+			link = links['href']
+			#print(links['href'])
+			if prices == None:
+				price = ""	
+				#print('\t' + "None")
 				continue
 			else:
-				print('\t' + cash.text)
+				price = prices.text
+				#print('\t' + prices.text)
 			if reviews == None:
-				print('\t' + "None")
+				#print('\t' + "None")
+				continue
 			else:
-				print('\t' + reviews.text)
-			#print(tmp)
+				review = reviews.text
+				#print('\t' + reviews.text)
+			c.execute("INSERT INTO products VALUES (?, ?, ?, ?)", (title, price, review, link))
+			db.commit()
 		url = list(url)
 		url[68] = str(i + 2)
 		url[90] = str(i + 2)
 		url = "".join(url)
 		driver.get(url)
-		print('\t' + "Page: " + str(i + 2))
+#		print('\t' + "Page: " + str(i + 2))
 except KeyboardInterrupt:
 	driver.close()
 	display.stop()
@@ -60,5 +70,4 @@ except KeyboardInterrupt:
 finally:
 	driver.close()
 	display.stop()
-	#vdisplay.stop()
 	f.close()
